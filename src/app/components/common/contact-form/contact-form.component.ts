ea1b96dc65acb184b442deb9e31714bdb56e8a2c
anand-hha <aanand@hhaexchange.com>
2025-05-03 00:09:09 +0530

    added contact form and clients section

---
+++ src/app/components/common/contact-form/contact-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { WasButtonComponent } from "../was-button/was-button.component";
+
+@Component({
+  selector: 'app-contact-form',
+  imports: [WasButtonComponent],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss'
+})
+export class ContactFormComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,10 +4,12 @@ import { ContactBannerComponent } from "../../components/home/contact-banner/con
 import { AboutComponent } from "../../components/home/about/about.component";
 import { IntroComponent } from '../../components/home/intro/intro.component';
 import { ServicesComponent } from "../../components/home/services/services.component";
+import { HappyClientsComponent } from "../../components/common/happy-clients/happy-clients.component";
+import { ContactFormComponent } from "../../components/common/contact-form/contact-form.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, IntroComponent, ContactBannerComponent, AboutComponent, ServicesComponent],
+  imports: [HeaderComponent, IntroComponent, ContactBannerComponent, AboutComponent, ServicesComponent, HappyClientsComponent, ContactFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
