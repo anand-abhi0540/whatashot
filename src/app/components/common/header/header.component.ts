3cad5c7879c388b0b097ca0004099b9e62533709
anand-hha <aanand@hhaexchange.com>
2025-06-10 01:01:45 +0530

    mobile responsive

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { WasButtonComponent } from '../../common/was-button/was-button.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, WasButtonComponent],
+  imports: [CommonModule, RouterLink, WasButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  constructor(private router: Router) {}
+
+  public menuClicked(clickedMenuItem: string): void {
+    console.log(clickedMenuItem);
+
+    this.isMenuOpen = false;
+    // if (clickedMenuItem === 'contact-us' || clickedMenuItem === 'quote-section') {
+    //   // const el = document.querySelector(`#${clickedMenuItem}`);
+    //   // if (el) {
+    //   //   (el as HTMLElement).scrollIntoView({ behavior: 'smooth' });
+    //   // }
+    // } else {
+    // }
+    this.router.navigate([clickedMenuItem]);
+  }
+}
